124a049261b86a7d803fb4b57c34f14bf16088bc
ThamizhselvanET <34275155+ThamizhselvanET@users.noreply.github.com>
2024-05-09 10:26:09 -0700

    Update main.tf

--- azurerm_modules/staging_slots/main.tf
+++ azurerm_modules/staging_slots/main.tf
@@ -1,9 +1,8 @@
 resource "azurerm_app_service_slot" "staging_slot" {
-  count               = var.staging_slot_enabled ? 1 : 0
   name                = "${var.app_service_name}-staging"
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_name    = azurerm_app_service.app_service.name
 
-  # Other configurations for the staging slot
+  app_service_name = azurerm_app_service.app_service.name
 }
