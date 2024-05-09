925db5d1c019924d133a82b40223c3a641728f61
ThamizhselvanET <34275155+ThamizhselvanET@users.noreply.github.com>
2024-05-09 10:30:58 -0700

    Update main.tf

--- azurerm_modules/virtual_network/main.tf
+++ azurerm_modules/virtual_network/main.tf
@@ -1,13 +1,15 @@
 resource "azurerm_virtual_network" "virtual_network" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
+  name                = var.virtual_network_name
   location            = var.location
   resource_group_name = var.resource_group_name
+  address_space       = var.virtual_network_address_space
 
-  # Other configurations for the virtual network
+  tags = var.virtual_network_tags
 }
 
-output "virtual_network_id" {
-  description = "ID of the deployed Virtual Network"
-  value       = azurerm_virtual_network.virtual_network.id
+resource "azurerm_subnet" "main" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = var.subnet_address_prefixes
 }
