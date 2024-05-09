bb8671c450b46e10c1557e7e2a19482f1e8f5f66
ThamizhselvanET <34275155+ThamizhselvanET@users.noreply.github.com>
2024-05-09 10:16:29 -0700

    Update variables.tf

--- azurerm_modules/variables.tf
+++ azurerm_modules/variables.tf
@@ -1,17 +1,59 @@
-variable "resource_group_name" {
-  description = "Name of the resource group where resources will be created"
+variable "location" {
+  description = "The Azure region to deploy the resources."
+  type        = string
 }
 
-variable "location" {
-  description = "Azure region where resources will be deployed"
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "app_service_plan_name" {
+  description = "The name of the App Service Plan."
+  type        = string
+}
+
+variable "app_service_plan_sku_tier" {
+  description = "The SKU tier of the App Service Plan."
+  type        = string
+}
+
+variable "app_service_plan_sku_size" {
+  description = "The SKU size of the App Service Plan."
+  type        = string
+}
+
+variable "service_plan.capacity" {
+  description = "The Capacity of the App Service Plan."
+  type        = string
+}
+
+variable "virtual_network_name" {
+  description = "The name of the Virtual Network."
+  type        = string
+}
+
+variable "virtual_network_address_space" {
+  description = "The address space of the Virtual Network."
+  type        = list(string)
+}
+
+variable "virtual_network_tags" {
+  description = "Tags to be applied to the Virtual Network."
+  type        = map(string)
+}
+
+variable "subnet_name" {
+  description = "The name of the Subnet."
+  type        = string
+}
+
+variable "subnet_address_prefixes" {
+  description = "The address prefixes of the Subnet."
+  type        = list(string)
 }
 
 variable "app_service_name" {
-  description = "Name of the Azure App Service"
-}
-
-variable "staging_slot_enabled" {
-  description = "Enable staging slots for the App Service"
-  type        = bool
-  default     = true
+  description = "The name of the App Service."
+  type        = string
 }
